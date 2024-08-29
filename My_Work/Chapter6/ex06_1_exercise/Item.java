14ed70640dba8d4ab74eb76ea39fd3160f873d03
Pierre <jonkerpierre637@gmail.com>
2024-08-29 14:50:08 +0200

    Chapter 6 Done

---
+++ My_Work/Chapter6/ex06_1_exercise/Item.java
@@ -0,0 +1,13 @@
+
+package Chapter6.ex06_1_exercise;
+
+public class Item {
+
+        public int itemID;
+        public String desc;
+        public double price ;
+        public int quantity;
+
+}
+
+
